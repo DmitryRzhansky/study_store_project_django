d4d6d884ed766748e661abf341b2ad4f314ccd93
rzh_exe <dima.rzhansky@gmail.com>
2025-09-05 10:36:03 +0500

    feat(goods): добавлена модель Products для товаров
    - создан класс Products в models.py приложения goods
    - поля модели:
        - name: CharField, уникальное название товара
        - slug: SlugField, URL для товара
        - description: TextField, описание товара
        - image: ImageField, временное хранение изображений через upload_to='goods_images'
        - price: DecimalField, цена товара
    - добавлены verbose_name для полей и модели
    - в Meta:
        - db_table = 'product'
        - verbose_name = 'Продукт'
        - verbose_name_plural = 'Продукты'
    - upload_to временный, позже будет реализовано хранение в другом месте

--- goods/models.py
+++ goods/models.py
@@ -9,3 +9,15 @@ class Categories(models.Model):
         db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение' )
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
