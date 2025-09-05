e85128daf6e5695efc173e445e37f1cee1f44013
rzh_exe <dima.rzhansky@gmail.com>
2025-09-05 09:09:11 +0500

    fix(goods): исправлен NoReverseMatch и подключено пространство имен catalog
    - добавлено app_name = 'catalog' в goods/urls.py
    - изменено подключение в app/urls.py: path('catalog/', include('goods.urls', namespace='catalog'))
    - во всех ссылках base.html заменено на namespace 'catalog'
    - функции catalog() и product() во views возвращают соответствующие шаблоны

--- app/urls.py
+++ app/urls.py
@@ -19,11 +19,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from main import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='goods')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
