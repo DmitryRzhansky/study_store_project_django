f8336ab3bc801c62c60345e4b68f1e95ae3cf065
rzh_exe <dima.rzhansky@gmail.com>
2025-09-09 16:47:08 +0500

    chore(db): переключение на PostgreSQL и пересоздание фикстур
    - settings.py: настроена база данных PostgreSQL
      - ENGINE: 'django.db.backends.postgresql'
      - NAME: 'shoes_store'
      - USER: 'shoes_user'
      - PASSWORD: '123'
      - HOST: 'localhost'
      - PORT: '5432'
    - Удалён db.sqlite3
    - Старые фикстуры удалены, созданы новые

--- app/settings.py
+++ app/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shoes_store',
+        'USER': 'shoes_user',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
